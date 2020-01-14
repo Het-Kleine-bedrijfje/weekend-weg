b6ccf59a533f5d9294086450fa156ad80ac84dcb
CapGuido <guido.kleine@capgemini.com>
2020-01-14 14:41:17 +0100

    implemented vuex and styling of steden page

--- src/locations/CitySelector.vue
+++ src/locations/CitySelector.vue
@@ -1,15 +1,16 @@
 <template>
-  <v-container cols="6">
+  <v-container>   
     <v-row class="voeg-toe-form" dark>
       <v-text-field v-model="nieuweStad" label="Stadsnaam"></v-text-field>
-      <v-btn dark bottom @click="AddNewCity(nieuweStad)">Toevoegen</v-btn>
+      <v-btn dark bottom @click="CheckArray(nieuweStad)">Toevoegen</v-btn>
     </v-row>
 
     <v-row align="center" class="d-md-flex">
-      <v-list>
+      <v-list width="500px">
         <template v-for="(stad, index) in steden">
           <v-list-item class="steden-lijst-item" :key="index">
             <v-list-item-content>{{ stad }}</v-list-item-content>
+            <v-btn x-small class="red" @click="DeleteItem(index)">X</v-btn>
           </v-list-item>
         </template>
       </v-list>
@@ -22,18 +23,41 @@ export default {
   name: "CitySelector",
   data() {
     return {
-      steden: ["Boedapest", "Edinbrough", "Oosterhout Camping", "Belgrado"],
-      nieuweStad: " "
+      nieuweStad: "",
+      stedenLijst: []
     };
   },
+  computed: {
+    steden() {
+      return this.$store.state.steden;
+    }
+  },
   methods: {
-    AddNewCity: nieuweStad => {
-      if (nieuweStad == "") {
+    AddCity(stad) {
+      // stad krijgt hoofdletter en voegt het daarna to aan de steden lijst
+      if (stad == "") {
         alert("Vul een stadsnaam in!");
       } else {
-        this.steden.push(nieuweStad);
+        this.$store.commit("AddNewCity", stad);
+        return (this.nieuweStad = "");
       }
+    },
+    /* eslint-disable no-console */
+    CheckArray(checkStad) {
+      var stad =
+        checkStad.charAt(0).toUpperCase() + checkStad.slice(1)
+
+      const stedenLijst = this.$store.state.steden;
+      if (stedenLijst.includes(stad)) {
+        alert(stad + " zit al in deze lijst!");
+      } else {
+        this.AddCity(stad);
+      }
+    },
+    DeleteItem(index) {
+      this.$store.commit("DeleteItem", index);
     }
+    /* eslint-enable no-console */
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,17 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    steden: ["Boedapest", "Edinbrough", "Oosterhout Camping", "Belgrado"],
+  },
+  mutations: {
+    AddNewCity(state, nieuweStad) {
+        state.steden.push(nieuweStad);
+    },
+    DeleteItem(state, index) {
+      state.steden.splice(index, 1);
+    }
+  },
   actions: {},
   modules: {}
 });
