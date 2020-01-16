280378de89187039a5944bc171c07088419cf83a
CapGuido <guido.kleine@capgemini.com>
2020-01-16 15:51:06 +0100

    Update CitySelector.vue

--- src/locations/CitySelector.vue
+++ src/locations/CitySelector.vue
@@ -2,14 +2,14 @@
   <v-container>
     <v-row class="voeg-toe-form" dark>
       <v-text-field v-model="nieuweStad" label="Stadsnaam"></v-text-field>
-      <v-btn dark bottom @click="CheckArray(nieuweStad)">Toevoegen</v-btn>
+      <v-btn dark bottom @click="AddCity(nieuweStad)">Toevoegen</v-btn>
     </v-row>
     <v-row align="center" class="d-md-flex">
       <v-list width="500px">
         <template v-for="(stad, index) in steden">
           <v-list-item class="steden-lijst-item" :key="index">
-            <v-list-item-content>{{ stad }}</v-list-item-content>
-            <v-btn x-small class="red" @click="DeleteItem(index)">X</v-btn>
+            <v-list-item-content>{{ stad.id }}</v-list-item-content>
+            <v-btn x-small class="red" @click="DeleteItem(stad)">X</v-btn>
           </v-list-item>
         </template>
       </v-list>
@@ -29,33 +29,43 @@ export default {
     };
   },
   /* eslint-disable no-console */
-  computed: {
-
-  },
+  computed: {},
   firestore: {
-    steden: locatieCol
+    steden: locatieCol.orderBy("stadsnaam")
   },
   methods: {
     AddCity(stad) {
       // stad krijgt hoofdletter en voegt het daarna to aan de steden lijst
-      if (stad == "") {
+      var nieuweStad = stad.charAt(0).toUpperCase() + stad.slice(1);
+      if (nieuweStad == "") {
         alert("Vul een stadsnaam in!");
       } else {
         // voeg locatie toe aan firestore
+        locatieCol
+          .doc(nieuweStad)
+          .set({
+            stadsnaam: nieuweStad
+          })
+          .then(function() {
+            console.log("Document created");
+          })
+          .catch(function(error) {
+            console.log("Error when creating document: " + error);
+          });
         return (this.nieuweStad = "");
       }
     },
-    CheckArray(checkStad) {
-      var stad = checkStad.charAt(0).toUpperCase() + checkStad.slice(1);
-      const stedenLijst = this.$store.state.steden;
-      if (stedenLijst.includes(stad)) {
-        alert(stad + " zit al in deze lijst!");
-      } else {
-        this.AddCity(stad);
-      }
-    },
-    DeleteItem(index) {
-      // delete locatie uit firestor
+    DeleteItem(stad) {
+      // delete locatie uit firestor     
+      locatieCol
+        .doc(stad.id)
+        .delete()
+        .then(function() {
+          console.log("Document successfully deleted!");
+        })
+        .catch(function(error) {
+          console.error("Error removing document: ", error);
+        });
     }
     /* eslint-enable no-console */
   }
